56286dd49fa819c14eb9fcdb7ec9571a4445ce6d
magnapax <chunhee.han@outlook.com>
2020-06-12 14:29:59 +0900

    ArrayList 활용
    ArrayList 활용 배열 생성
    발전된 for문 활용

--- src/array/HouseTestArrayList.java
+++ src/array/HouseTestArrayList.java
@@ -1,8 +1,28 @@
 package array;
 
+import java.util.ArrayList;
+
 public class HouseTestArrayList {
 
 	public static void main(String[] args) {
+
+		ArrayList<House> houseArrayList = new ArrayList<House>();
+
+		houseArrayList.add(new House("우리집", "한옥"));
+		houseArrayList.add(new House("영희집", "미국"));
+		houseArrayList.add(new House("친구1", "중국"));
+		houseArrayList.add(new House("친구2", "일본"));
+		houseArrayList.add(new House("친구3", "벳남"));
+
+		System.out.println("==Using regular for==");
+		for (int i = 0; i < houseArrayList.size(); i++) {
+			System.out.println(houseArrayList.get(i).showHouseInfo());
+		}
+
+		System.out.println("\n==Using Enhanced for==");
+		for (House arrHouse : houseArrayList) {
+			System.out.println(arrHouse.showHouseInfo());
+		}
 
 	}
 

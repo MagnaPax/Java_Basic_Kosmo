12269fd76a4f8a67765d6271c5ce2543b55ce926
MagnaPax <chunhee.han@outlook.com>
2020-06-18 17:17:34 +0900

    java.lang 의 Math 클래스

---
+++ src/chapter11_class/Test02_01_Math.java
@@ -0,0 +1,21 @@
+package chapter11_class;
+
+public class Test02_01_Math {
+
+	public static void main(String[] args) {
+
+		// 2의 8승
+		System.out.println("Math.pow(2, 8) : " + Math.pow(2, 8));
+
+		// 0.0과 1.0사이의 난수 출력
+		System.out.println("Math.random() : " + Math.random());
+
+		// 40과 30중 작은값 출력
+		System.out.println("Math.min(40, 30) : " + Math.min(40, 30));
+
+		// 40과 30중 큰 값 출력
+		System.out.println("Math.max(40, 30) : " + Math.max(40, 30));
+
+	}
+
+}

b536fd919617de9587526037ca4b3721cee42f65
MagnaPax <chunhee.han@outlook.com>
2020-06-26 10:23:51 +0900

    컴파일러에 의해 예외가 체크되는 경우 - exception
    이 파일을 실행하려고 하면 실행 전 compile error 발생. 빨간 줄 그어진 부분에 마우스를 올리면 두 개의 추천이 나타남.

---
+++ src/exception/FileinputExceptionEx.java
@@ -0,0 +1,13 @@
+package exception;
+
+import java.io.FileInputStream;
+
+public class FileinputExceptionEx {
+
+	public static void main(String[] args) {
+
+		FileInputStream fis = new FileInputStream("d:/a.txt");
+
+	}
+
+}
